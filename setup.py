eb798c13c045810c6173ef8c3830948fac0d5ca8
Gordon Morehouse <gordon@morehouse.me>
2012-06-27 22:15:09 -0700

    license, notice, setup.py reqs

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='saltbox',
+    version='0.1',
+    description='Simple Password Storage',
+    long_description=readme,
+    author='Gordon Morehouse <gordon@morehouse.me>',
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires = [
+        'passlib==1.6',
+        'SQLAlchemy==0.7.8',
+        'celery==2.5.5',
+    ],
+    tests_require = [
+    ],
+)
